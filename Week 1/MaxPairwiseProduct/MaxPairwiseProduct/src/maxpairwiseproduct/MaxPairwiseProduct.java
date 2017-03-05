77deabe716696df92e58853033ccae09267845a7
Rana Ahmed <ranaahmed554@gmail.com>
2017-03-05 15:35:03 +0200

    add stress test

--- Week 1/MaxPairwiseProduct/MaxPairwiseProduct/src/maxpairwiseproduct/MaxPairwiseProduct.java
+++ Week 1/MaxPairwiseProduct/MaxPairwiseProduct/src/maxpairwiseproduct/MaxPairwiseProduct.java
@@ -34,16 +34,44 @@ public class MaxPairwiseProduct {
         result = (long)firstmax * secondmax;
         return result;
     }
+    
+    static void stressTest() {
+        Random rand = new Random();
+        while (true) {
+            int n = rand.nextInt(11) + 2;
+            System.out.println(n);
+            int[] a = new int[n];
+            for (int i = 0; i < n; ++i) {
+              a[i] = rand.nextInt(100000);
+            }
+            for (int i = 0; i < n; ++i) {
+                System.out.println(a[i]);
+            }
+            
+            long res1 = getMaxPairwiseProduct(a);
+            long res2 = getMaxPairwiseProductFast(a);
+            if (res1 != res2) {
+                System.out.println("Wrong answer: " + res1 + " " + res2);
+              break;
+            }
+            else {
+                System.out.println("OK");
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
+        /*FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
         int[] numbers = new int[n];
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
         System.out.println(getMaxPairwiseProduct(numbers));
-         System.out.println(getMaxPairwiseProductFast(numbers));
+        System.out.println(getMaxPairwiseProductFast(numbers));*/
+        
+        //stress test
+        stressTest();
     }
 
     static class FastScanner {
