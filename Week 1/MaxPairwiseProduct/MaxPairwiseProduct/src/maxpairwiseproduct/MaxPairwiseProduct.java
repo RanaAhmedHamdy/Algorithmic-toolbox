c2b09735b399847879a812c1f81f5deb5c2a91e8
Rana Ahmed <ranaahmed554@gmail.com>
2017-03-05 13:54:26 +0200

    removed nested loops to make algorithm faster

--- Week 1/MaxPairwiseProduct/MaxPairwiseProduct/src/maxpairwiseproduct/MaxPairwiseProduct.java
+++ Week 1/MaxPairwiseProduct/MaxPairwiseProduct/src/maxpairwiseproduct/MaxPairwiseProduct.java
@@ -8,10 +8,30 @@ public class MaxPairwiseProduct {
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
                 if ((long)numbers[i] * numbers[j] > result) {
+                    //multiplication should be forced to use long arthimetic, onlu casting result to long is not enough
                     result = (long)numbers[i] * numbers[j];
                 }
             }
         }
+        return result;
+    }
+    
+    static long getMaxPairwiseProductFast(int[] numbers) {
+        long result = 0;
+        int n = numbers.length;
+        int firstmax = 0;
+        int secondmax = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            if(firstmax < numbers[i]) {
+                secondmax = firstmax;
+                firstmax = numbers[i];
+            } else if(secondmax < numbers[i]) {
+                secondmax = numbers[i];
+            }
+        }
+        
+        result = (long)firstmax * secondmax;
         return result;
     }
 
@@ -23,6 +43,7 @@ public class MaxPairwiseProduct {
             numbers[i] = scanner.nextInt();
         }
         System.out.println(getMaxPairwiseProduct(numbers));
+         System.out.println(getMaxPairwiseProductFast(numbers));
     }
 
     static class FastScanner {
